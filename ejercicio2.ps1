56e30551781d7a2aa06b4eb46a30b60734530879
MauricioVigo <mauriciovigo@gmail.com>
2021-12-05 00:03:14 +0100

    Primer intento

---
+++ ejercicio2.ps1
@@ -0,0 +1,23 @@
+﻿
+
+
+$x=Read-Host "Ingresa el nombre de usuario"
+$y=Read-Host "Cual es la contraseña"
+$z=ConvertTo-SecureString "$y" -AsPlainText -Force
+$w=Get-LocalUser | where-Object Name -eq "$x$i" | Measure
+
+for ($i = 1; $i -lt 6; $i++)
+{ 
+if ((get-localuser "$x $i") -eq false)
+{
+   New-LocalUser "$x $i" -Password $z 
+}
+else
+{
+    
+}
+    
+}
+
+
+
