60219c026e7319fee822196d0df7594ca4fb9dd2
Mauricio Vigo Lavalle <mauriciovigo@gmail.com>
2021-12-04 22:24:18 +0100

    Add-Scripts

---
+++ README.md
@@ -0,0 +1,2 @@
+# Powershell Scripts
+Scripts del instituto
---
+++ ejercicio 2 pwshell.ps1
@@ -0,0 +1,10 @@
+﻿$Usuario=Read-Host ("Escribe el nombre del nuevo usuario")
+
+
+for ($i = 1; $i -lt 7; $i++)
+{ 
+    $comando="$usuario $i"
+    mkdir $comando
+}
+
+g
