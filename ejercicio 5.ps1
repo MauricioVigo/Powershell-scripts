235cb39850e33d4211f945e6f6a4393e00a1339e
MauricioVigo <mauriciovigo@gmail.com>
2021-12-06 20:48:21 +0100

    ejercicio 5

---
+++ ejercicio 5.ps1
@@ -0,0 +1,17 @@
+﻿$x=read-Host "Cual es el nombre del archivo"
+if ((test-path $x) -like "false")
+{
+    Write-Host "El fichero no existe"
+}
+else
+{
+    switch ($x)
+{
+    "b" {}
+    "m" {}
+    Default {}
+}
+}
+
+
+
