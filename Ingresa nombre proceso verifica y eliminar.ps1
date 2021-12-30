f097bc1dd937e885831295d58a16c8313cf3d7e9
MauricioVigo <mauriciovigo@gmail.com>
2021-12-31 00:55:47 +0100

    Script para buscar y eliminar proceso

---
+++ Ingresa nombre proceso verifica y eliminar.ps1
@@ -0,0 +1,21 @@
+﻿$x=Read-Host "Indica el nombre del proceso a eliminar"
+
+    if ((Get-Process | Where-Object ProcessName -Match $x).count -eq 1)
+    {
+        Stop-Process -name $x
+    }
+    else
+    {
+        Write-Host "el proceso no existe, por lo cual no puede ser eliminado"
+    }
+
+
+
+
+
+
+
+
+
+
+
