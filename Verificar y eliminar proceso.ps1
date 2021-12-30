9e617849ef3450215e5c68dd5eb3f6fb8342c443
MauricioVigo <mauriciovigo@gmail.com>
2021-12-30 22:13:47 +0100

    Script para verificar proceso y eliminarlo

---
+++ Verificar y eliminar proceso.ps1
@@ -0,0 +1,24 @@
+﻿Write-Host "deaseas eliminar el proceso de la calculadora"
+$y=read-host "Indica SI o No"
+
+if ($y -eq "si")
+{
+    if ((Get-Process | Where-Object ProcessName -Match calculator).count -eq 1)
+    {
+        Stop-Process -name calculator
+    }
+    else
+    {
+        Write-Host "el proceso no existe, por lo cual no puede ser eliminado"
+    }
+}
+
+
+
+
+
+
+
+
+
+
